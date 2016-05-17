9487540c11c68c1d32655fca3c768e7b331ad333
Jhunu <jhunu.fernandes@gmail.com>
2016-05-17 15:16:28 -0300

    Correção de código e simplificação de programa

--- ArduWatchRaspSerial.py
+++ ArduWatchRaspSerial.py
@@ -1,3 +1,9 @@
+# @Author: Jhunu Fernandes Araújo <windsor>
+# @Date:   17-05-2016
+# @Email:  jhunu@gmail.com
+# @Last modified by:   windsor
+# @Last modified time: 17-05-2016
+
 # Programa que envia os dados à placa
 
 import serial
@@ -5,36 +11,18 @@ from datetime import datetime
 import time
 
 def int_str(leitura):
-	if (leitura == '0'):
-		return('00':
-	elif (leitura == '1')
-		return('01')
-	elif (leitura == '2')
-		return('02')
-	elif (leitura == '3')
-		return('03')
-	elif (leitura == '4')
-		return('04')
-	elif (leitura == '5')
-		return('05')
-	elif (leitura == '6')
-		return('06')
-	elif (leitura == '7')
-		return('07')
-	elif (leitura == '8')
-		return('08')
-	elif (leitura == '9')
-		return('09')
+	if int(leitura) < 10:
+		return('0'+leitura)
 	else:
 		return(str(leitura))
 
 ser = serial.Serial('/dev/ttyUSB0',9600);
 
-while (1):
+while True:
 	t = datetime.now()
 	hour = t.hour
 	minutes = t.minute
-	hour = str(hour)
-	minute = str(minutes)
+	hour = int_str(hour)
+	minute = int_str(minutes)
 	ser.write(hour+minutes)
 	time.sleep(2)
